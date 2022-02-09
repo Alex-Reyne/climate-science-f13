c8a12a16b797020b575dd97595abbb7ade236ae5
Alex-Reyne <creativereyne@gmail.com>
2022-02-08 18:28:01 -0600

    create fade animation

--- components/Question.jsx
+++ components/Question.jsx
@@ -3,13 +3,15 @@ import styles from './Question.module.css';
 
 export default function Question() {
 	return (
-		<div className={styles.container}>
-			<h3 className={styles.question}>
-				Which of the below statements about electricity is not true?
-			</h3>
-			<Answer text={`Electricity is measured in units called watts`} />
-			<Answer text={`Electricity flows at the speed of light`} />
-			<Answer text={`Electricity is a primary energy source`} />
+		<div className={styles.fadein}>
+			<div className={styles.container}>
+				<h3 className={styles.question}>
+					Which of the below statements about electricity is not true?
+				</h3>
+				<Answer text={`Electricity is measured in units called watts`} />
+				<Answer text={`Electricity flows at the speed of light`} />
+				<Answer text={`Electricity is a primary energy source`} />
+			</div>
 		</div>
 	);
 }
