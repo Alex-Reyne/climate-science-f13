75ed5df3deb710fb2460ed1d8e2d330eb10291d6
Alex-Reyne <creativereyne@gmail.com>
2022-02-08 21:20:01 -0600

    destructure props for Answer.jsx component

--- components/Answer.jsx
+++ components/Answer.jsx
@@ -1,11 +1,11 @@
 import styles from './Answer.module.css';
 
-export default function Answer(props) {
+export default function Answer({ text, correct }) {
 	return (
 		<div className={styles.scroll}>
 			<div className={styles.container}>
-				<div className={props.correct ? styles.correct : styles.circle}></div>
-				<h3 className={styles.text}>{props.text}</h3>
+				<div className={correct ? styles.correct : styles.circle}></div>
+				<h3 className={styles.text}>{text}</h3>
 			</div>
 		</div>
 	);
